2c2adbff65827b07017641037c4bbc1650aa7d8a
YuTengjing <ytj2713151713@gmail.com>
2019-06-23 21:33:16 +0800

    extract the function responseFile and add last-modified cache

--- src/index.js
+++ src/index.js
@@ -7,29 +7,38 @@ const Path = require('path');
 const app = new Koa();
 const router = new Router();
 
+const responseFile = async (path, context, encoding) => {
+    const fileContent = await fs.readFile(path, encoding);
+    context.type = mime.getType(path);
+    context.body = fileContent;
+};
+
 // 处理首页
 router.get(/(^\/index(.html)?$)|(^\/$)/, async (ctx, next) => {
-    ctx.type = mime.getType('.html');
-
-    const content = await fs.readFile(Path.resolve(__dirname, './index.html'), 'UTF-8');
-    ctx.body = content;
-
+    await responseFile(Path.resolve(__dirname, './index.html'), ctx, 'UTF-8');
     await next();
 });
 
 // 处理图片
 router.get(/\S*\.(jpe?g|png)$/, async (ctx, next) => {
-    const { response, path } = ctx;
-    ctx.type = mime.getType(path);
-
+    const { request, response, path } = ctx;
     response.set('pragma', 'no-cache');
-    // max-age 值是精确到秒，设置过期时间为 1 分钟
-    response.set('cache-control', `max-age=${1 * 60}`);
-    // 添加 expires 字段到响应头，过期时间 2 分钟
-    response.set('expires', new Date(Date.now() + 2 * 60 * 1000).toString());
 
-    const imageBuffer = await fs.readFile(Path.resolve(__dirname, `.${path}`));
-    ctx.body = imageBuffer;
+    // max-age 值是精确到秒，设置过期时间为 1 分钟
+    // response.set('cache-control', `max-age=${1 * 60}`);
+    // 添加 expires 字段到响应头，过期时间 2 分钟
+    // response.set('expires', new Date(Date.now() + 2 * 60 * 1000).toString());
+
+    const imagePath = Path.resolve(__dirname, `.${path}`);
+    const ifModifiedSince = request.headers['if-modified-since'];
+    const imageStatus = await fs.stat(imagePath);
+    const lastModified = imageStatus.mtime.toGMTString();
+    if (ifModifiedSince === lastModified) {
+        response.status = 304;
+    } else {
+        response.lastModified = lastModified;
+        await responseFile(imagePath, ctx);
+    }
 
     await next();
 });
@@ -37,11 +46,7 @@ router.get(/\S*\.(jpe?g|png)$/, async (ctx, next) => {
 // 处理 css 文件
 router.get(/\S*\.css$/, async (ctx, next) => {
     const { path } = ctx;
-    ctx.type = mime.getType(path);
-
-    const content = await fs.readFile(Path.resolve(__dirname, `.${path}`), 'UTF-8');
-    ctx.body = content;
-
+    await responseFile(Path.resolve(__dirname, `.${path}`), ctx, 'UTF-8');
     await next();
 });
 
