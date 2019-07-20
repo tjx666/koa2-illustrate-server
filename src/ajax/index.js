0475112e87dd5f269a69e4afebd04663f20e7c17
YuTengjing <ytj2713151713@gmail.com>
2019-07-21 00:03:08 +0800

    :sparkles: add test GET route in ajax

---
+++ src/ajax/index.js
@@ -0,0 +1,27 @@
+const Koa = require('koa');
+const logger = require('koa-logger');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+const chalk = require('chalk');
+
+const server = new Koa();
+const router = new Router();
+
+router.get('/test/get', async (ctx, next) => {
+    ctx.body = `Test GET request success!`;
+    await next();
+});
+
+server.use(logger());
+server.use(bodyParser());
+server.use(router.routes());
+
+const PORT = 1027;
+const HOST = 'localhost';
+const serverURL = `http://${HOST}:${PORT}/`;
+server.listen(PORT, HOST);
+console.log(chalk.yellow('Server is running at:'), chalk.bold.underline.bgBlueBright.gray(serverURL));
+
+process.addListener('unhandledRejection', (error) => {
+    console.error(error);
+});
